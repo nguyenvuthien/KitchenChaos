d1e623a433c6ccf35dc5520ace68687e55c67d97
nguyenvuthien <thien180102@gmail.com>
2024-03-10 22:59:48 +0700

    Initial commit

---
+++ Assets/Scripts/UI/ProgressBarUI.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBarUI : MonoBehaviour
+{
+    [SerializeField] private GameObject hasProgressGameObject;
+    [SerializeField] private Image barImage;
+
+    private IHasProgress hasProgress;
+    private void Start()
+    {
+        hasProgress = hasProgressGameObject.GetComponent<IHasProgress>();
+        if (hasProgress == null)
+        {
+            Debug.LogError("Game object " + hasProgressGameObject + "does not have a component that implements IHasProgress!");
+        }
+        hasProgress.OnProgressChanged += HasProgress_OnProgressChanged;
+        barImage.fillAmount = 0f;
+
+        Hide();
+    }
+
+    private void HasProgress_OnProgressChanged(object sender, IHasProgress.OnProgressChangedEnventArgs e)
+    {
+        barImage.fillAmount = e.progressNormalized;
+
+        if (e.progressNormalized == 0f || e.progressNormalized == 1f)
+        {
+            Hide();
+        }
+        else
+        {
+            Show();
+        }
+    }
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
