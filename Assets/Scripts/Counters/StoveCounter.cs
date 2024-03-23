38e32fa79968eb533f0c30d72e7c212fbac7f7e8
nguyenvuthien <thien180102@gmail.com>
2024-03-23 17:20:19 +0700

    Sync Plate Add Ingredients

--- Assets/Scripts/Counters/StoveCounter.cs
+++ Assets/Scripts/Counters/StoveCounter.cs
@@ -157,9 +157,9 @@ public class StoveCounter : BaseCounter, IHasProgress
                     //Player is holding a Plate
                     if (plateKitchenObject.TryAddIngredient(GetKitchenObject().GetKitchenObjectSO()))
                     {
-                        GetKitchenObject().DestroySelf();
+                        KitchenObject.DestroyKitchenObject(GetKitchenObject());
 
-                        state.Value = State.Idle;
+                        SetStateIdleServerRpc();
                     }
                 }
             }
