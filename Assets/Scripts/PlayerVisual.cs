ad1f065ddb8fe095d12a01e4e56da859e493ee10
nguyenvuthien <thien180102@gmail.com>
2024-05-21 21:22:46 +0700

    Player Asset UI

--- Assets/Scripts/PlayerVisual.cs
+++ Assets/Scripts/PlayerVisual.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class PlayerVisual : MonoBehaviour
 {
-    [SerializeField] private MeshRenderer headMeshRerender;
-    [SerializeField] private MeshRenderer bodyMeshRerender;
+    //[SerializeField] private MeshRenderer headMeshRerender;
+    //[SerializeField] private MeshRenderer bodyMeshRerender;
+    [SerializeField] private MeshRenderer apronMeshRerender;
+    [SerializeField] private MeshRenderer pocketMeshRerender;
 
     private Material material;
 
     private void Awake()
     {
-        material = new Material(headMeshRerender.material);
-        headMeshRerender.material = material;
-        bodyMeshRerender.material = material;
+        material = new Material(apronMeshRerender.material);
+        //headMeshRerender.material = material;
+        //bodyMeshRerender.material = material;
+        apronMeshRerender.material = material;
+        pocketMeshRerender.material = material;
+
     }
 
     public void SetPlayerColor(Color color)
